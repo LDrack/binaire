adf2fb3475d2c00d281a5b319652335ea8005148
LDrack <39444912+LDrack@users.noreply.github.com>
2022-04-28 17:46:07 +0200

    Added DB integration, finished auto mode

---
+++ binaire/Migrations/20220428081349_initialMigration.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace binaire.Migrations
+{
+    public partial class initialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Boards",
+                columns: table => new
+                {
+                    BoardId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    BoardSpecifier = table.Column<int>(type: "int", nullable: false),
+                    BoardId1 = table.Column<int>(type: "int", nullable: false),
+                    BoardId2 = table.Column<int>(type: "int", nullable: false),
+                    BoardId3 = table.Column<int>(type: "int", nullable: false),
+                    Description = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Boards", x => x.BoardId);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Readings",
+                columns: table => new
+                {
+                    ReadingId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    BoardId = table.Column<int>(type: "int", nullable: true),
+                    PufStart = table.Column<int>(type: "int", nullable: false),
+                    PufEnd = table.Column<int>(type: "int", nullable: false),
+                    Temperature = table.Column<float>(type: "float", nullable: false),
+                    Timestamp = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    Fingerprint = table.Column<byte[]>(type: "longblob", maxLength: 10000, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Readings", x => x.ReadingId);
+                    table.ForeignKey(
+                        name: "FK_Readings_Boards_BoardId",
+                        column: x => x.BoardId,
+                        principalTable: "Boards",
+                        principalColumn: "BoardId");
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Readings_BoardId",
+                table: "Readings",
+                column: "BoardId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Readings");
+
+            migrationBuilder.DropTable(
+                name: "Boards");
+        }
+    }
+}
