21be0c4128272670b0396896da91f43a6c5fdd0e
LDrack <39444912+LDrack@users.noreply.github.com>
2022-06-05 11:53:14 +0200

    Final status after all evaluations

---
+++ binaire/ExportData.cs
@@ -0,0 +1,22 @@
+﻿//////  Binaire - Utility for SRAM PUF analysis
+  ////  Lucas Drack
+    //  2022-05-10
+
+using CsvHelper;
+using System.Text;
+
+namespace binaire
+{
+    public static class ExportData
+    {
+        // Replaced own implementation with CsvHelper library
+        public static void WriteCsv<T>(List<T> genericList, string fileName)
+        {
+            using (var writer = new StreamWriter(fileName))
+            using (var csv = new CsvWriter(writer, System.Globalization.CultureInfo.InvariantCulture))
+            {
+                csv.WriteRecords(genericList);
+            }
+        }
+    }
+}
